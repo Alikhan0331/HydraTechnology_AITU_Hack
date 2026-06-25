e314a670c35936cc6ed446afac10f113f69388b5
Alikhan0331 <alilya.3108@gmail.com>
2026-06-25 15:04:17 +0500

    initial: FastAPI + React project setup

---
+++ backend/app/main.py
@@ -0,0 +1,17 @@
+# backend/app/main.py
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # React (Vite)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "HydraTechnology API is running!"}
